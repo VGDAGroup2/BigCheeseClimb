737722f231833392682d92733d7644556902eb26
Reiden <reideng_flip@yahoo.com>
2014-04-23 17:31:33 -0700

    MainCharacter Class Comments
    Put comments in the MainCharacter class to improve readability.

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-Added the removal of objects. If you want to remove an object add it to the output queue in Runnable Object.
-
-Made the size of the platforms 1; The image will take up the rest of the space. We will just be colliding with the top (Our line).
-
-Added the GameState class. It will help us track the state of the game and keep static fields.
-
-Added W and up arrow to the jumping mechanism.
-
-Moved the Platform controls to the correct package.
+Commented the MainCharacter class, specifically the jumping portion.
+Changes will be made later(most likely thursday night) to clean and improve the code.
--- src/com/game/code/graphics/MainCharacter.java
+++ src/com/game/code/graphics/MainCharacter.java
@@ -13,7 +13,7 @@ import com.game.code.collision.CollisionDetection;
 
 public class MainCharacter extends RunnableObject {
 	
-	private boolean isFalling = false;
+	private boolean isFalling = false; //May change to isGrounded instead later.
 	private boolean isJumping = false;
 	private double speed = 2;
 	private double maxJump = 10;
@@ -28,56 +28,57 @@ public class MainCharacter extends RunnableObject {
 	}
 	
 	public void update() {
-		if(CollisionDetection.detectColission(this) != null){
+		if(CollisionDetection.detectColission(this) != null){ //Checks to see if it is colliding with something.
 			isFalling = false;
 		}
 		else if(!isJumping){
-			isFalling = true;
+			isFalling = true; //If it isn't colliding with anything and it isn't jumping then it is falling.
 		}
 		
+		//Player Control -- Jump
 		if(!isFalling){	
-			if(Control.jump){
-				if(!isJumping)
+			if(Control.jump){                       //Checks to see if it isn't falling.
+				if(!isJumping)						//If it isn't falling then it checks if it isn't already jumping. If it isn't set the jump to 5.
 					jump = 5;
 				isJumping = true;
 			}
-			if(isJumping){
-				if(jump < maxJump && jump > 0){
-					y-= jump;
-					jump += jumpSpeed;
-					if(jump < (maxJump / 2) && !slowed)
-						jumpSpeed += 0.05;
-					else if(jump > (maxJump / 2)){
-						slowed = true;
+			if(isJumping){										//If the player isJumping then
+				if(jump < maxJump && jump > 0){					//if the jump amount is less than the maximum height it can jump to and jump isn't a negative number
+					y-= jump;									//the player will jump.
+					jump += jumpSpeed;							//jumpSpeed is to give it the effect that it slows down as it gets to the peak of the jump.
+					if(jump < (maxJump / 2) && !slowed)			//if the player hasn't reached the peak(slowed) and the jump height is 
+						jumpSpeed += 0.05;						//less than half the maximum jump height then the player is speed up.
+					else if(jump > (maxJump / 2)){				//else if it is higher than half the maximum jump height it will start to slow down.
+						slowed = true;							//slowed is set to true here
 						jumpSpeed -= 0.05;
 					}
 				}
 				else{
-					jumpSpeed = 0;
-					isFalling = true;
+					jumpSpeed = 0;								//Player isn't jumping anymore so jump speed is set to 0.
+					isFalling = true;							//and the player is now falling.
 				}
 			}
 				
 		}
-		else{
-			y+=jump;
-			if(isJumping){
-				jump += jumpSpeed;
+		else{													//The player is falling
+			y+=jump;			
+			if(isJumping){										//If the player jumped then the second part of the jump is activated.
+				jump += jumpSpeed;								//What happens is the speed at which the player falls increases until it his the cap 0.2.
 				if(jumpSpeed < 0.2)
 					jumpSpeed += 0.05;
-			}
-			if(CollisionDetection.detectColission(this) !=null){
-				isJumping = false;
+			}														
+			if(CollisionDetection.detectColission(this) !=null){	//If player is colliding with something then it is no longer jumping.
+				isJumping = false;									//and jumpSpeed is reset to its original value.
 				jumpSpeed = 0.2;
-				jump = 1.1;
-			}
-			else if(jump == 1.1)
-				jump = 5;
+				jump = 1.1;											//jump is set to 1.1 here because if it falling at the same rate as the platforms the 
+			}														//collision isn't detected.
+			else if(jump == 1.1)									//if the player walks off the platform 
+				jump = 5;											//then the jump is reset to 5.
 		}
-		
+		//Player Control -- Move Left and Right
 		if(Control.left){x-=speed;}
 		if(Control.right){x+=speed;}
-		if(x <= 0 || x >= Screen.width)
+		if(x <= 0 || x >= Screen.width) //Moves between left and right sides of the screen.
 			x = Screen.width - x;
 	}
 
